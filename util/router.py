faab0da15be8ac2b3e718733d8745669845c4138
Kaleb Lanz <kglanz2005@icloud.com>
2025-09-05 14:09:13 -0400

    router.route_request()  HOPING is okay

--- util/router.py
+++ util/router.py
@@ -26,6 +26,7 @@ class Router:
         
 
     def route_request(self, request, handler):
+        print(f"self.route:{self.route}")
         '''
         * takes a request object and a TCPHandler object (returns nothing)
         
@@ -36,6 +37,8 @@ class Router:
         request_path = request.path
         request_method_path = request_method + " " + request_path
 
+        func_called_bool = False
+
         '''
         with the request line, see if it matches(exact or 'start substring' ) with any of the keys in the dict
         possible inputs:
@@ -43,6 +46,33 @@ class Router:
         /public/image/image.png
         
         '''
+        if request_method_path in self.route:
+            #this means that the path given by Request object is an added route
+            func = self.route[request_method_path][0]
+            bool_path = self.route[request_method_path][1]
+            func_called_bool = True
+            print(f"the route from input request:{request_method_path}")
+            func(request,handler)
+        
+        
+        for request_line in self.route:
+             if re.match(request_line,request_method_path) != None:
+                  bool_path = self.route[request_line][1]
+                  if bool_path == False:
+                       func = self.route[request_line][0]
+                       func(request,handler)
+                       func_called_bool = True
+                       print(f"the route from input request object:{request_method_path}      the request in self.route:{request_line}")
+                       break
+        
+        if func_called_bool == False:
+            print("404 will be called!!!!!")
+            #return 404
+                       
+
+
+
+
         '''
         #this should deal for the multiple image type case
         if re.match("/public/image/",request_method_path) != None:
@@ -69,14 +99,95 @@ class Router:
 
 
 def test1():
-        router = Router()
-        request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
-        router.add_route("GET","/public/image", None, False)
-        print(router.route)
-        router.route_request(request,None)
+    #404 error!
+    router = Router()
+    request = Request(b'GET /public/image HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","/public/image/dinosaour.png", action_function, False)
+    print(router.route)
+    router.route_request(request,None)
+        
+
+def test2():
+    #404 should be called
+    router = Router()
+    request = Request(b'GET /public/image HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+
+def test3():
+    #404 should be called
+    router = Router()
+    request = Request(b'GET /public/image HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("GET","image.html",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+
+def test4():
+    #404 should be called
+    router = Router()
+    request = Request(b'GET /public/dog.html HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("GET","image.html",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+
+def test5():
+    #the route from input request object:GET /public/image/eagle.jpg      the request in self.route:GET /public/image/
+    router = Router()
+    request = Request(b'GET /public/image/eagle.jpg HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("GET","/public/image/",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+
+def test6():
+    #404 error
+    router = Router()
+    request = Request(b'GET /public/image/eagle.jpg HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("GET","/public/image/",action_function,True)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+
+def test7():
+    #404 error
+    router = Router()
+    request = Request(b'GET /public/image/ HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("POST","/public/image/",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
+    
+def test8():
+    #the route from input request object:GET /public/image/      the request in self.route:GET /public/image/
+    router = Router()
+    request = Request(b'GET /public/image/ HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
+    router.add_route("GET","chat.html",action_function,False)
+    router.add_route("GET","dog.html",action_function,False)
+    router.add_route("GET","/public/image/",action_function,False)
+    router.add_route("GET","/public/",action_function,False)
+    print(f"router.route:{router.route}")
+    router.route_request(request, None)
 
 
+def action_function(request, handler):
+    pass
+     
     
 if __name__ == '__main__':
-        test1()
+    #test1()
+    #test2()
+    #test3()
+    #test4()
+    #test5()
+    #test6()
+    #test7()
+    test8()
     
